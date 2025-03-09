dc65827a46a07eb005a372e5d121451a985a5f08
780748976 <780748976@qq.com>
2025-03-09 15:07:40 +0800

    为用户概率类型增加注释

--- PoJo/src/main/java/com/sky/pojo/constant/UserChangeProbabilityType.java
+++ PoJo/src/main/java/com/sky/pojo/constant/UserChangeProbabilityType.java
@@ -1,12 +1,19 @@
 package com.sky.pojo.constant;
 
 public class UserChangeProbabilityType {
+    //播放
     public static final int PLAY = 0;
+    //点赞
     public static final int LIKE = 1;
+    //取消点赞
     public static final int UNLIKE = 2;
+    //点踩
     public static final int DISLIKE = 3;
+    //取消点踩
     public static final int UNDISLIKE = 4;
+    //收藏
     public static final int FAVORITE = 5;
+    //取消收藏
     public static final int UNFAVORITE = 6;
     //点赞转点踩
     public static final int LIKE_TO_DISLIKE = 7;
